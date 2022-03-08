21147125ef37407cbb14fc0ac69bd8c6dff90690
Xavier Hamel <82169676+ItsMeXav@users.noreply.github.com>
2022-03-08 14:53:29 -0500

    misssing file finished dataRefresh

--- Application/child/dataRefresh/dataRefresh.ts
+++ Application/child/dataRefresh/dataRefresh.ts
@@ -9,7 +9,6 @@ async function start(chain:ChainClass, dex:{[addresses:string]:DEXData},updateLi
     //Initialise the provider
     var activeChain = chain;
 
-    console.log("Number of dex",Object.keys(dex).length);
 
     //Fill active Dex
     for(var [address, dexEntrie] of Object.entries(dex)){
@@ -22,30 +21,23 @@ async function start(chain:ChainClass, dex:{[addresses:string]:DEXData},updateLi
         var test = 0;
         changedDex.forEach((address)=>{
             priceUpdateArray.push(exchanges[address].update())
-            console.log("test:",test);
             test ++;
         }); 
         return await Promise.all(priceUpdateArray);
     }
 
-    console.log("yeess-1");
-    console.log("active chain length",Object.keys(activeDex))
     let updateArray = await updateEverything(activeDex,Object.keys(activeDex));
-    console.log("yeess-1,5");
     if(updateArray.length > 0){
         updateListener(activeChain.provider.blockNumber, updateArray);
     }
 
-    console.log("yess 0")
 
     let topic0 = [ethers.utils.id("Swap(address,uint256,uint256,uint256,uint256,address)"),ethers.utils.id("Mint(address,uint256, uint256)"),ethers.utils.id("Burn(address,uint256, uint256)")];
 
-    console.log("yeess");
 
     //Start update on every new block
     activeChain.provider.on("block",async (blockHeight)=>{
 
-        console.log("Test 123");
         
         let filter:ethers.providers.Filter ={
             
@@ -57,9 +49,6 @@ async function start(chain:ChainClass, dex:{[addresses:string]:DEXData},updateLi
         }
 
         let logs = await activeChain.provider.getLogs(filter);
-
-        console.log("haha",logs);
-        
         
         let changedDex:string[] = [];
         logs.forEach((log)=>{
@@ -68,14 +57,9 @@ async function start(chain:ChainClass, dex:{[addresses:string]:DEXData},updateLi
                 changedDex.push(address);
             }
         });
-        console.log("Test 123",changedDex);
-        //console.log(`Logs for block ${blockHeight}:`, logs);
-        //console.log(`Block #${blockHeight}:`,block)
 
         let updateArray = await updateEverything(activeDex,changedDex); 
-        console.log("block:", blockHeight, updateArray.length);
         if(updateArray.length > 0){
-            console.log("updateArray:",updateArray);
             updateListener(blockHeight, updateArray);
         }
     });
